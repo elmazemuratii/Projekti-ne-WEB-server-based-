cd6cacc4e8bad750e695041fc59e9c4d88573569
Jora Axhami <jora.axhami@student.uni-pr.edu>
2026-04-26 22:36:47 +0200

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -1,1 +1,25 @@
+<?php
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+if(!isset($_SESSION['user'])){
+    header("Location: login.php");
+    exit;
+}
+
+
+if(!isset($_SESSION['role'])){
+    $_SESSION['role'] = "user";
+}
+
+
+function requireAdmin(){
+    if(!isset($_SESSION['role']) || $_SESSION['role'] != "admin"){
+        echo "<p style='color:red;text-align:center;'>Access denied: Admin only</p>";
+        exit;
+    }
+}
+?>
