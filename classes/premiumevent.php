019d25e940036e46e36ca688e836def99c47be68
EraKerceli <era.kerceli@student.uni-pr.edu>
2026-04-27 23:24:41 +0200

    Update premiumevent.php

--- classes/premiumevent.php
+++ classes/premiumevent.php
@@ -1,1 +1,22 @@
+<?php
+require_once "Event.php";
 
+class PremiumEvent extends Event {
+
+    private $price;
+
+    public function __construct($title, $city, $date, $maxParticipants, $price) {
+        parent::__construct($title, $city, $date, $maxParticipants);
+        $this->price = $price;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+
+    public function display() {
+        return parent::display() . " | Premium: $" . $this->price;
+    }
+}
+?>
