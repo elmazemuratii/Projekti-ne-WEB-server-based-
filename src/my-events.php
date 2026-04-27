b94b472fdd28847bf136f0da5a5a37e181b02ba6
EraKerceli <era.kerceli@student.uni-pr.edu>
2026-04-27 17:00:59 +0200

    Update my-events.php

--- src/my-events.php
+++ src/my-events.php
@@ -1,1 +1,33 @@
 
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/nav.php'; ?>
+
+<?php
+
+if(!isset($_SESSION['user'])){
+    echo "<p style='color:red;text-align:center;'>Duhet me u logu për me pa eventet!</p>";
+    include 'includes/footer.php';
+    exit;
+}
+
+
+if(!isset($_SESSION['my_events']) || !is_array($_SESSION['my_events'])){
+    $_SESSION['my_events'] = [];
+}
+
+
+if(isset($_GET['delete'])){
+    $index = (int) $_GET['delete'];
+
+    if(isset($_SESSION['my_events'][$index])){
+        unset($_SESSION['my_events'][$index]);
+        $_SESSION['my_events'] = array_values($_SESSION['my_events']);
+    }
+}
+?>
